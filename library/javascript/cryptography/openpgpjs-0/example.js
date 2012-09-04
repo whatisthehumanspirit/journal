f947882c591aa42832d01ffa8be771ce0e30c0d7
Alexis Carlson <whatisthehumanspirit@gmail.com>
2012-09-03 21:31:24 -0700

    Remove JavaScript crypto libraries to ~/Dev.

--- library/javascript/cryptography/openpgpjs-0/example.js
+++
@@ -1,5 +0,0 @@
-function encrypt() {
-  openpgp.init();
-  var pub_key = openpgp.read_publicKey($('#pubkey').text());
-  $('#message').val(openpgp.write_encrypted_message(pub_key,$('#message').val()));
-}
